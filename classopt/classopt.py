6fff89278604c54af4a4735e5845b5fd14b34ef5
moisutsu <moisutsu@gmail.com>
2021-07-04 14:21:57 +0900

    Add advanced usage

--- classopt/classopt.py
+++ classopt/classopt.py
@@ -8,9 +8,26 @@ def ClassOpt(cls):
         parser = ArgumentParser()
 
         for arg_name, arg_field in cls.__dataclass_fields__.items():
-            parser.add_argument(
-                f"--{arg_name}", type=arg_field.type, **arg_field.metadata
-            )
+            kwargs = {}
+            kwargs.update(arg_field.metadata)
+            kwargs["type"] = arg_field.type
+
+            name_or_flags = []
+            if "name_or_flags" in arg_field.metadata:
+                name_or_flags.append(arg_field.metadata["name_or_flags"])
+                if arg_field.metadata["name_or_flags"].startswith("-"):
+                    name_or_flags.append(f"--{arg_name}")
+                kwargs.pop("name_or_flags")
+            else:
+                name_or_flags.append(f"--{arg_name}")
+
+            if "action" in arg_field.metadata:
+                kwargs.pop("type")
+
+            if "type" in arg_field.metadata:
+                kwargs["type"] = arg_field.metadata["type"]
+
+            parser.add_argument(*name_or_flags, **kwargs)
 
         args = parser.parse_args()
         return cls(**vars(args))
--- tests/test_classopt.py
+++ tests/test_classopt.py
@@ -1,6 +1,7 @@
 import unittest
 import sys
 
+from dataclasses import field
 from classopt import ClassOpt
 
 
@@ -11,14 +12,18 @@ class Opt:
     arg_float: float
 
 
+@ClassOpt
+class AdvancedUsageOpt:
+    without_hyphen: str = field(metadata={"name_or_flags": "without_hyphen"})
+    short_arg: str = field(metadata={"name_or_flags": "-s"})
+    default_int: int = field(metadata={"default": 3})
+    store_true: bool = field(metadata={"action": "store_true"})
+    nargs: list = field(metadata={"nargs": "+", "type": int})
+
+
 class TestClassOpt(unittest.TestCase):
     def test_classopt(self):
-        sys.argv.append("--arg_int")
-        sys.argv.append("5")
-        sys.argv.append("--arg_str")
-        sys.argv.append("hello")
-        sys.argv.append("--arg_float")
-        sys.argv.append("3.2")
+        set_args("--arg_int", "5", "--arg_str", "hello", "--arg_float", "3.2")
 
         opt = Opt.from_args()
 
@@ -26,4 +31,35 @@ class TestClassOpt(unittest.TestCase):
         assert opt.arg_str == "hello"
         assert opt.arg_float == 3.2
 
-        del sys.argv[1:]
+        del_args()
+
+    def test_advanced_usage(self):
+        set_args(
+            "without_hyphen",
+            "-s",
+            "short_arg",
+            "--store_true",
+            "--nargs",
+            "1",
+            "2",
+            "3",
+        )
+
+        opt = AdvancedUsageOpt.from_args()
+
+        assert opt.without_hyphen == "without_hyphen"
+        assert opt.short_arg == "short_arg"
+        assert opt.default_int == 3
+        assert opt.store_true
+        assert opt.nargs == [1, 2, 3]
+
+        del_args()
+
+
+def set_args(*args):
+    for arg in args:
+        sys.argv.append(arg)
+
+
+def del_args():
+    del sys.argv[1:]
